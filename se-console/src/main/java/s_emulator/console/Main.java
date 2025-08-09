603ab8c6fc638a6bb8ed36b06cee74f5681ae103
fabershaun <fabershaun@gmail.com>
2025-08-09 15:00:20 +0300

    Initialize project structure with console and engine modules

---
+++ se-console/src/main/java/s_emulator/console/Main.java
@@ -0,0 +1,7 @@
+package s_emulator.console;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main is running in " + Main.class.getPackageName());
+    }
+}
---
+++ se-engine/src/main/java/s_emulator/engine/Main.java
@@ -0,0 +1,7 @@
+package s_emulator.engine;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main is running in " + Main.class.getPackageName());
+    }
+}
